c608dde2bc4c6c4cbdb7fa6e5cd8d2cbca42d219
mkla14 <mkamp93@gmail.com>
2020-03-09 20:40:05 +0100

    Added Unity project to repository

---
+++ TowerDefenseProject/Assets/TD_Scripts/Shop.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+
+    public TurretBlueprint standardTurret;
+    public TurretBlueprint missileTurret;
+    public TurretBlueprint laserTurret;
+    
+    BuildManager buildManager;
+
+    void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
+
+  public void SelectStandardTurret()
+    {
+        Debug.Log("#Standard Turret Purchased");
+        buildManager.SelectTurretToBuild(standardTurret);
+    }
+    
+    public void SelectMissileTurret()
+    {
+        Debug.Log("#Missile Turret Purchased");
+        buildManager.SelectTurretToBuild(missileTurret);
+    }
+
+    public void SelectLaserTurret()
+    {
+        Debug.Log("#Laser Turret Purchased");
+        buildManager.SelectTurretToBuild(laserTurret);
+    }
+
+
+}
