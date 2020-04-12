6fc1b8a869cb07f84fe25b53b482dd22804983b2
mkla14 <mkamp93@gmail.com>
2020-04-12 11:01:36 +0200

    UI Stuff

---
+++ Assets/TD_Scripts/GameOver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public Text roundsText;
+
+    private void OnEnable()
+    {
+        roundsText.text = PlayerStats.Rounds.ToString();
+    }
+
+    public void Retry()
+    {
+        //Load Active Scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        
+        //Load Scene by name
+        //SceneManager.LoadScene("MainScene");
+
+        //Load Scene by build index
+        //SceneManager.LoadScene(0);
+    }
+
+    public void Menu()
+    {
+        Debug.Log("Go To Menu");
+    }
+
+}
